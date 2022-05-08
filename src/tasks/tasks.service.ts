357dcc565b9d8880c7b494411d77a2398e10eb80
mmal73 <luisito250899@gmail.com>
2022-05-08 18:26:20 -0500

    feat(Tasks): get all tasks using the service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [
+    {
+      id: 1,
+      task: 'Lorem',
+    },
+    {
+      id: 2,
+      task: 'Lorem',
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
