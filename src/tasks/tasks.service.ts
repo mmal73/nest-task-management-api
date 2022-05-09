2eb0d2f0f1991e81c661273294bb1670040b49db
mmal73 <luisito250899@gmail.com>
2022-05-09 13:57:11 -0500

    feat(Tasks): adding error 404 when task with id not exists

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -44,7 +44,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const taskFound = this.tasks.find((task) => task.id === id);
+    if (!taskFound) {
+      throw new NotFoundException();
+    }
+    return taskFound;
   }
 
   deleteTask(id: string): void {
