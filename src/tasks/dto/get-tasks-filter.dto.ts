5cdaada59b635e966d1b744c7b400a06e1b7fe93
mmal73 <luisito250899@gmail.com>
2022-05-09 14:20:52 -0500

    feat(Tasks): adding validationPipe for search filters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,14 @@
 import { TaskStatus } from '../task.model';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus, {
+    message: 'This status is not valid',
+  })
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
