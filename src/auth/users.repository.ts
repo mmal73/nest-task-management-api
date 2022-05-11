a582c80552c0a5b9eccfb6d9e90b25053bad7b92
mmal73 <luisito250899@gmail.com>
2022-05-11 14:52:39 -0500

    feat(Auth): register users

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == '23505') {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException('Username already exists');
+    }
+  }
+}
